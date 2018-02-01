4dfc9c0d517be1a31bc29e2c1695912370ad7828
plgcwiertni <cwiertnia.michal@gmail.com>
2018-02-01 12:39:35 +0100

    VFS-3622 Refactored creation of config files.

--- scripts/environment/node.py
+++ scripts/environment/node.py
@@ -2,48 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import os
-
-OFFSET = 4
+import textwrap
 
 
 def create_node_config_file(deployment_dir, service_name, node_name, apps):
     with open(os.path.join(deployment_dir, service_name,
                            '{}-overlay.config'.format(node_name)), 'w') as f:
-        # TODO:
-        offset = 0
-        f.write('[\n')
-        offset += OFFSET
-        f.write(offset * ' ' + '{onepanel, [\n')
-        offset += 4
-        for app in apps[:-1]:
-            write_line_to_config_file(app, f, offset)
-        write_line_to_config_file(apps[-1], f, offset, last_app=True)
-        offset -= 4
-        f.write(offset * ' ' + ']}\n')
-        offset -= 4
-        f.write('].\n')
+        apps_configs = []
+
+        for app in apps:
+            apps_configs.append(create_config_for_app(app))
+
+        last_comma = apps_configs[-1].rfind(',')
+        apps_configs[-1] = (apps_configs[-1][:last_comma] +
+                            apps_configs[-1][last_comma + 1:])
+
+        f.write('''
+[
+    {{onepanel, [
+{}
+    ]}}
+].
+'''.format(textwrap.indent('\n\n'.join(apps_configs), '        ')))
 
 
-def write_line_to_config_file(app, file, offset, last_app=False):
-    if not last_app:
-        for attr, val in app.config.items():
-            # TODO:
-            if 'panel' not in app.name:
-                attr = '{}_{}'.format(app.name, attr)
-            file.write(offset * ' ' + '{{{0}, "{1}"}},\n'.format(attr, val))
-        file.write('\n')
-    else:
-        for attr, val in list(app.config.items())[:-1]:
-            # TODO:
-            if 'panel' not in app.name:
-                attr = '{}_{}'.format(app.name, attr)
-            file.write(offset * ' ' + '{{{0}, "{1}"}},\n'.format(attr, val))
+def create_config_for_app(app):
+    app_config = []
 
-        attr, val = list(app.config.items())[-1]
-        # TODO:
+    for attr, val in app.config.items():
         if 'panel' not in app.name:
             attr = '{}_{}'.format(app.name, attr)
-        file.write(offset * ' ' + '{{{0}, "{1}"}}\n'.format(attr, val))
+        app_config.append('{{{0}, "{1}"}},'.format(attr, val))
 
-
-
+    return '\n'.join(app_config)
