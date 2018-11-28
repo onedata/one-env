776be1787df2d73aa5e024aa37a4765dc4a7c5f6
plgcwiertni <cwiertni@student.agh.edu.pl>
2018-11-28 14:31:26 +0100

    VFS-5118 Added worker, panel and cluster-manager options to watch and update. Delete option for rsync is now optional.

--- scripts/onenv_update.py
+++ scripts/onenv_update.py
@@ -11,6 +11,7 @@ import os
 import glob
 import argparse
 import subprocess
+from typing import List
 
 import kubernetes
 
@@ -18,25 +19,34 @@ from .utils.k8s import pods, helm
 from .utils.yaml_utils import load_yaml
 from .utils import terminal, arg_help_formatter
 from .utils.one_env_dir import deployments_dir, user_config
+from .utils.names_and_paths import (APP_OP_PANEL, APP_OZ_PANEL, APP_ONEZONE,
+                                    APP_ONEPROVIDER, APP_CLUSTER_MANAGER)
 
 
-DIRS_TO_SYNC = ['_build/default/rel/*/data/gui_static', '_build/default/lib',
-                'src', 'include']
+GUI_DIRS_TO_SYNC = ['_build/default/rel/*/data/gui_static']
+BACKEND_DIRS_TO_SYNC = ['_build/default/lib', 'src', 'include']
+ALL_DIRS_TO_SYNC = GUI_DIRS_TO_SYNC + BACKEND_DIRS_TO_SYNC
 
 
-def update_sources_for_component(pod_name: str, source_path: str) -> None:
-    for dir_to_sync in DIRS_TO_SYNC:
+def update_sources_for_component(pod_name: str, source_path: str,
+                                 dirs_to_sync: List[str],
+                                 delete: bool = False) -> None:
+    for dir_to_sync in dirs_to_sync:
         dir_path = os.path.join(source_path, dir_to_sync)
         for expanded_path in glob.glob(dir_path):
             destination_path = '{}:{}'.format(pod_name,
                                               os.path.dirname(expanded_path))
 
             if os.path.exists(expanded_path):
-                subprocess.call(pods.rsync_cmd(expanded_path,
-                                               destination_path), shell=True)
+                rsync_cmd = pods.rsync_cmd(expanded_path, destination_path,
+                                           delete)
+                subprocess.call(rsync_cmd, shell=True)
 
 
-def update_sources_in_pod(pod: kubernetes.client.V1Pod) -> None:
+def update_sources_in_pod(pod: kubernetes.client.V1Pod,
+                          sources_to_update: List[str],
+                          dirs_to_sync: List[str],
+                          delete: bool = False) -> None:
     deployment_dir = deployments_dir.get_current_deployment_dir()
     deployment_data_path = os.path.join(deployment_dir, 'deployment_data.yml')
     try:
@@ -50,13 +60,16 @@ def update_sources_in_pod(pod: kubernetes.client.V1Pod) -> None:
         pod_cfg = deployment_data.get('sources').get(pod_name)
 
         if pod_cfg:
-            for source_path in pod_cfg.values():
-                update_sources_for_component(pod_name, source_path)
+            for source_name, source_path in pod_cfg.items():
+                if source_name in sources_to_update:
+                    update_sources_for_component(pod_name, source_path,
+                                                 dirs_to_sync, delete)
 
 
-def update_deployment() -> None:
+def update_deployment(sources_to_update: List[str], dirs_to_sync: List[str],
+                      delete: bool = False) -> None:
     for pod in pods.list_pods():
-        update_sources_in_pod(pod)
+        update_sources_in_pod(pod, sources_to_update, dirs_to_sync, delete)
 
 
 def main() -> None:
@@ -73,15 +86,80 @@ def main() -> None:
         dest='pod'
     )
 
+    update_args_parser.add_argument(
+        '-d', '--delete',
+        help='specifies if rsync should delete files in pods when they are '
+             'deleted on host',
+        action='store_true'
+    )
+
+    update_args_parser.add_argument(
+        '-p', '--panel',
+        action='store_true',
+        help='update sources for onepanel service',
+    )
+
+    update_args_parser.add_argument(
+        '-c', '--cluster-manager',
+        action='store_true',
+        help='update sources for cluster-manager service',
+    )
+
+    update_args_parser.add_argument(
+        '-w', '--worker',
+        action='store_true',
+        help='update sources for (op|oz)-worker',
+    )
+
+    update_args_parser.add_argument(
+        '-a', '--all',
+        action='store_true',
+        help='update sources for all services',
+    )
+
+    sources_type = update_args_parser.add_mutually_exclusive_group()
+
+    sources_type.add_argument(
+        '-g', '--gui',
+        action='store_const',
+        help='update sources only for GUI',
+        const=GUI_DIRS_TO_SYNC,
+        dest='dirs_to_sync'
+    )
+
+    sources_type.add_argument(
+        '-b', '--backend',
+        action='store_const',
+        help='update sources only for backend',
+        const=BACKEND_DIRS_TO_SYNC,
+        dest='dirs_to_sync'
+    )
+
     update_args = update_args_parser.parse_args()
 
     user_config.ensure_exists()
     helm.ensure_deployment(exists=True, fail_with_error=True)
 
+    sources_to_update = []
+
+    if update_args.panel:
+        sources_to_update.extend([APP_OZ_PANEL, APP_OP_PANEL])
+    if update_args.worker:
+        sources_to_update.extend([APP_ONEZONE, APP_ONEPROVIDER])
+    if update_args.cluster_manager:
+        sources_to_update.extend([APP_CLUSTER_MANAGER])
+    if update_args.all:
+        sources_to_update = [APP_OZ_PANEL, APP_OP_PANEL, APP_ONEZONE,
+                             APP_ONEPROVIDER, APP_CLUSTER_MANAGER]
+
+    dirs_to_sync = update_args.dirs_to_sync or ALL_DIRS_TO_SYNC
+
     if update_args.pod:
-        pods.match_pod_and_run(update_args.pod, update_sources_in_pod)
+        pods.match_pod_and_run(update_args.pod, update_sources_in_pod,
+                               sources_to_update, dirs_to_sync,
+                               update_args.delete)
     else:
-        update_deployment()
+        update_deployment(sources_to_update, dirs_to_sync, update_args.delete)
 
 
 if __name__ == '__main__':
