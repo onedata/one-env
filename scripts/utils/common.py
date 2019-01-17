1693a0413c20fef5b6ddcd7669ff6b78d64a3588
plgcwiertni <cwiertni@student.agh.edu.pl>
2019-01-17 15:23:35 +0100

    VFS-5188 Fixed replacing in file

--- scripts/utils/common.py
+++ scripts/utils/common.py
@@ -18,11 +18,21 @@ def random_string(chars_num: int) -> str:
                    range(chars_num))
 
 
-def replace_in_file(file_path: str, pattern: str, value: str,
-                    backup: str = '', regexp: bool = False) -> None:
+def replace_in_file_using_fileinput(file_path: str, pattern: str, value: str,
+                                    backup: str = '',
+                                    regexp: bool = False) -> None:
     with fileinput.FileInput(file_path, inplace=True, backup=backup) as file:
         for line in file:
             if regexp:
-                print(re.sub(pattern, value, line), end='')
+                print(re.sub(pattern, value, line), end='', flush=True)
             else:
-                print(line.replace(pattern, value), end='')
+                print(line.replace(pattern, value), end='', flush=True)
+
+
+def replace_in_file_using_open(file_path, pattern, value):
+    with open(file_path, 'r+') as file:
+        content = file.read()
+        content = re.sub(pattern, value, content)
+        file.seek(0)
+        file.truncate()
+        file.write(content)
